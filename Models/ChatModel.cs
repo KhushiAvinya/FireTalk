94ba2afe7480fda02b792beae71195e7c14ef206
khushi <khushi.avinya@gmail.com>
2025-10-17 15:49:35 +0530

    Notification and home page chnages

--- Models/ChatModel.cs
+++ Models/ChatModel.cs
@@ -1,10 +1,5 @@
 ﻿using FireTalk.Components.Layout;
 using Google.Cloud.Firestore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FireTalk.Models
 {
